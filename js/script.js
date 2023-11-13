b6646ff2fcfec12f8171b4855a909752c0c7804f
Zehra Çelik <zehra.celikk2002@gmail.com>
2023-11-14 00:51:41 +0300

    movie details page was made

--- js/script.js
+++ js/script.js
@@ -48,6 +48,54 @@ async function displayPopularShows() {
     });
 }
 
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1];
+    const movie = await fetchAPIData(`/movie/${movieId}`);
+    const div = document.createElement('div');
+    
+    div.innerHTML = `
+    <div class="details-top">
+          <div>
+            <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+                ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map(genre => `<li class="list-group-item">${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary"> Budget: </span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes </li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+            ${movie.production_companies.map(company => `<li class="list-group-item">${company.name}</li>`).join('')}
+          </div>
+        </div>
+    `;
+    document.querySelector('#movie-details').appendChild(div);
+}
+
 async function fetchAPIData(endpoint) {
     const API_KEY = 'ab60d473df10890068aa790aaed730b8';
     const API_URL = 'https://api.themoviedb.org/3/';
@@ -80,6 +128,10 @@ function highlightActiveLink() {
     });
 }
 
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function init() {
     switch(global.currentPage) {
         case '/':
@@ -90,7 +142,7 @@ function init() {
             displayPopularShows();
             break;
         case'/movie-details.html':
-            console.log('Movie Details');
+            displayMovieDetails();
             break;
         case '/tv-details.html':
             console.log('TV Details');
