abb8ab2e2a0ca5c447e938086666df446fa2e76c
Zehra Çelik <zehra.celikk2002@gmail.com>
2023-11-15 00:56:17 +0300

    details page backdrop was added

--- js/script.js
+++ js/script.js
@@ -51,6 +51,9 @@ async function displayPopularShows() {
 async function displayMovieDetails() {
     const movieId = window.location.search.split('=')[1];
     const movie = await fetchAPIData(`/movie/${movieId}`);
+
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
     
     div.innerHTML = `
@@ -94,6 +97,28 @@ async function displayMovieDetails() {
         </div>
     `;
     document.querySelector('#movie-details').appendChild(div);
+}
+
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.classList.add('background-image');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/w1280${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if(type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv)
+    } else {
+            document.querySelector('#show-details').appendChild(overlayDiv)
+    }
 }
 
 async function fetchAPIData(endpoint) {
